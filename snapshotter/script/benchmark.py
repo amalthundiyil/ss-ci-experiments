61f517435f282d491179f24f5efb0e2c5bb49600
Amal Thundiyil <mail@amalthundiyil.com>
2024-10-18 12:53:27 +0200

    asdf
    Signed-off-by: Amal Thundiyil <mail@amalthundiyil.com>

--- snapshotter/script/benchmark.py
+++ snapshotter/script/benchmark.py
@@ -9,27 +9,37 @@ import os
 import shutil
 import pprint
 
+
 def run_benchmark(image, snapshotter, task):
-    print(f"Running benchmark with snapshotter: {snapshotter}, image: {image}, task: {task}")
+    print(
+        f"Running benchmark with snapshotter: {snapshotter}, image: {image}, task: {task}"
+    )
     benchmark_start = datetime.datetime.now()
     pull_start = datetime.datetime.now()
     subprocess.run(f"sudo nerdctl pull --snapshotter={snapshotter} {image}", shell=True)
     pull_end = datetime.datetime.now()
     run_start = datetime.datetime.now()
-    result = subprocess.run(f"""sudo nerdctl run --rm  --snapshotter={snapshotter} {image} /bin/bash -c "\
+    result = subprocess.run(
+        f"""sudo nerdctl run --rm  --snapshotter={snapshotter} {image} /bin/bash -c "\
 echo container_start: "'$(date -Ins)'"
 {task}
 echo container_end: "'$(date -Ins)'"
 "
-""", shell=True, capture_output=True, text=True)
+""",
+        shell=True,
+        capture_output=True,
+        text=True,
+    )
 
     run_end = datetime.datetime.now()
     output = result.stdout
-    container_start_match = re.search(r'container_start: ([\d\-T:\.\+]+)', output)
-    container_end_match = re.search(r'container_end: ([\d\-T:\.\+]+)', output)
+    container_start_match = re.search(r"container_start: ([\d\-T:\.\+]+)", output)
+    container_end_match = re.search(r"container_end: ([\d\-T:\.\+]+)", output)
 
     if not container_start_match or not container_end_match:
-        sys.exit("Error: container_start or container_end timestamp not found in the output.")
+        sys.exit(
+            "Error: container_start or container_end timestamp not found in the output."
+        )
 
     container_start = datetime.datetime.fromisoformat(container_start_match.group(1))
     container_end = datetime.datetime.fromisoformat(container_end_match.group(1))
@@ -47,8 +57,9 @@ echo container_end: "'$(date -Ins)'"
         "pull_time": pull_time,
         "creation_time": creation_time,
         "execution_time": execution_time,
-        "total_time": total_time
+        "total_time": total_time,
     }
+
 
 def perf_regression(old_results, new_results, threshold=0.05):
     for key in old_results:
@@ -62,70 +73,102 @@ def perf_regression(old_results, new_results, threshold=0.05):
     return False
 
 
-
 def cleanup():
     # stop services
-    services = ['containerd', 'cvmfs-snapshotter', 'autofs']
+    services = ["containerd", "cvmfs-snapshotter", "autofs"]
     for service in services:
         try:
-            subprocess.run(['sudo', 'systemctl', 'stop', service], check=True)
+            subprocess.run(["sudo", "systemctl", "stop", service], check=True)
         except subprocess.CalledProcessError as e:
             print(f"Error stopping {service}: {e.stderr}")
 
     # unmount
     try:
-        subprocess.run(['sudo', 'umount', "/cvmfs/unpacked.cern.ch"], check=True)
+        subprocess.run(["sudo", "umount", "/cvmfs/unpacked.cern.ch"], check=True)
     except subprocess.CalledProcessError as e:
         print(f"Error unmounting /cvmfs/unpacked.cern.ch: {e.stderr}")
 
     # clear cache
     def clear_cache(path):
-        if os.path.exists(path):
-            shutil.rmtree(path)
-        os.makedirs(path, mode=0o755, exist_ok=True)
+        try:
+            if os.path.exists(path):
+                subprocess.run(["sudo", "rm", "-rf", path], check=True)
+        except subprocess.CalledProcessError as e:
+            print(f"Error removing {path}: {e.stderr}")
+
+        try:
+            subprocess.run(["sudo", "mkdir", "-p", path], check=True)
+            subprocess.run(["sudo", "chmod", "755", path], check=True)
+        except subprocess.CalledProcessError as e:
+            print(f"Error creating {path}: {e.stderr}")
 
     clear_cache("/var/lib/containerd")
     clear_cache("/var/lib/containerd-cvmfs-grpc")
 
     try:
-        subprocess.run("sudo cvmfs_config reload -c", shell=True, check=True, capture_output=True, text=True)
+        subprocess.run(
+            "sudo cvmfs_config reload -c",
+            shell=True,
+            check=True,
+            capture_output=True,
+            text=True,
+        )
     except subprocess.CalledProcessError as e:
         print(f"Error reloading cvmfs_config: {e.stderr}")
 
     # start services
-    services = ['containerd', 'cvmfs-snapshotter', 'autofs']
+    services = ["containerd", "cvmfs-snapshotter", "autofs"]
     for service in services:
         try:
-            subprocess.run(['sudo', 'systemctl', 'start', service], check=True)
+            subprocess.run(["sudo", "systemctl", "start", service], check=True)
         except subprocess.CalledProcessError as e:
             print(f"Error starting {service}: {e.stderr}")
 
     # mount and check
     try:
-        subprocess.run(['sudo', 'mount', '-t', 'cvmfs', "unpacked.cern.ch", "/cvmfs/unpacked.cern.ch"], check=True)
+        subprocess.run(
+            [
+                "sudo",
+                "mount",
+                "-t",
+                "cvmfs",
+                "unpacked.cern.ch",
+                "/cvmfs/unpacked.cern.ch",
+            ],
+            check=True,
+        )
     except subprocess.CalledProcessError as e:
         print(f"Error mounting unpacked.cern.ch: {e.stderr}")
 
     try:
-        subprocess.run("sudo cvmfs_config probe", shell=True, check=True, capture_output=True, text=True)
+        subprocess.run(
+            "sudo cvmfs_config probe",
+            shell=True,
+            check=True,
+            capture_output=True,
+            text=True,
+        )
     except subprocess.CalledProcessError as e:
         print(f"Error probing cvmfs_config: {e.stderr}")
 
-    cvmfs_sock_exists = os.path.exists("/run/containerd-cvmfs-grpc/containerd-cvmfs-grpc.sock")
+    cvmfs_sock_exists = os.path.exists(
+        "/run/containerd-cvmfs-grpc/containerd-cvmfs-grpc.sock"
+    )
     if not cvmfs_sock_exists:
         raise AssertionError("containerd-cvmfs-grpc.sock does not exist.")
 
+
 if __name__ == "__main__":
     images = ["rootproject/root:6.32.02-ubuntu24.04"]
-    data = [ 
+    data = [
         {
             "image": "rootproject/root:6.32.02-ubuntu24.04",
             "tasks": [
-                "/bin/bash", 
-                "python -c 'print(\"# Hello World\")'", 
-                "python -c 'import ROOT; print(\"# import root complete\")'", 
-                "python /opt/root/tutorials/pyroot/fillrandom.py"
-            ]
+                "/bin/bash",
+                "python -c 'print(\"# Hello World\")'",
+                "python -c 'import ROOT; print(\"# import root complete\")'",
+                "python /opt/root/tutorials/pyroot/fillrandom.py",
+            ],
         }
     ]
 
@@ -134,11 +177,11 @@ if __name__ == "__main__":
     results = []
 
     for entry in data:
-        image, tasks = entry['image'], entry['tasks']
+        image, tasks = entry["image"], entry["tasks"]
         for task in tasks:
             results.append(run_benchmark(image, snapshotter, task))
             cleanup()
-    
+
     pprint.pprint(results)
 
     # if len(sys.argv) >= 2:
@@ -147,5 +190,5 @@ if __name__ == "__main__":
     #     if perf_regression(old_results, new_results):
     #         sys.exit(1)
 
-    with open('benchmark_results.json', 'w') as f:
+    with open("benchmark_results.json", "w") as f:
         json.dump(results, f, indent=4)
