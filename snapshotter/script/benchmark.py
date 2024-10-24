1eba07313ec0f3af5810081b09cfa37131743d13
Amal Thundiyil <mail@amalthundiyil.com>
2024-10-24 16:33:56 +0200

    asdf
    Signed-off-by: Amal Thundiyil <mail@amalthundiyil.com>

--- snapshotter/script/benchmark.py
+++ snapshotter/script/benchmark.py
@@ -26,9 +26,9 @@ def run_benchmark(iteration, image, snapshotter, task):
             echo pull_end: $(date +%s%N); \
             echo run_start: $(date +%s%N); \
             sudo nerdctl run --snapshotter={snapshotter} {image} /bin/bash -c "\
-            echo container_start: \$(date +%s%N); \
+            echo container_start: $(date +%s%N); \
             {task}; \
-            echo container_end: \$(date +%s%N)"; \
+            echo container_end: $(date +%s%N)"; \
             echo run_end: $(date +%s%N)
         """,
         shell=True,
